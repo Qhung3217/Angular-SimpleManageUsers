a0944225f94a292ae6f66a8db149bac587516b96
Qhung3217 <quanghung165200@gmail.com>
2022-06-13 11:42:46 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { ListComponent } from './users/list/list.component';
+import { UserComponent } from './users/list/user/user.component';
+import { EditComponent } from './users/edit/edit.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UsersComponent,
+    ListComponent,
+    UserComponent,
+    EditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
